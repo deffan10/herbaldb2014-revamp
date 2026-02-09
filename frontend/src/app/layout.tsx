d94aea6f114990829cd44e4bbb51e8bc711da6c0
Server ThinkSystem <febriandeffan@gmail.com>
2026-02-09 13:01:01 +0700

    Google Analytic

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -21,6 +21,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZVY9ES3XP0"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-ZVY9ES3XP0');
+            `,
+          }}
+        />
+      </head>
       <body className={`${inter.variable} font-sans antialiased`}>
         <Providers>{children}</Providers>
       </body>
